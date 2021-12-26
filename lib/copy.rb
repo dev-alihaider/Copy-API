19892daf8ccfa88dbf5d7378f3e216ec3287b465
= <=>
2021-12-26 10:00:39 +0500

    refactoring

--- lib/copy.rb
+++ lib/copy.rb
@@ -1,23 +1,22 @@
+# Class to handle copy related data from airtable and formatting values
+
 class Copy
   def refresh
     client = Airtable::Client.new(ENV['AIRTABLE_API_KEY'])
-    table = client.table(ENV['AIRTABLE_APP_ID'], "Copy")
-
+    table = client.table(ENV['AIRTABLE_APP_ID'], 'Copy')
     records = table.records
 
     file = File.read('./copy.json')
     file_data = JSON.parse(file).with_indifferent_access
-
     data_hash = parse_row file_data, records, ActiveSupport::HashWithIndifferentAccess.new
 
     while records.offset.present?
-      records = table.records(:offset => records.offset)
+      records = table.records(offset: records.offset)
       data_hash = parse_row file_data, records, data_hash
     end
 
     File.write('./copy.json', JSON.dump(data_hash))
-
-    $file_data = data_hash
+    $file_data = data_hash # I am using global variable or else I need to load file on every request
   end
 
   def format_value(params)
@@ -28,11 +27,9 @@ class Copy
       params.each do |k, v|
         if "{#{k}}".in? value
           value.gsub!("{#{k}}", v)
-        elsif "{#{k}, datetime}".in? value
-          # we need to make this kind of checks for custom formatting as per requirements
+        elsif "{#{k}, datetime}".in? value # we need to make this kind of checks for custom formatting as per requirements
           t = Time.at(v.to_i)
-          # it may show different time than written in doc due to timezone difference
-          value.gsub!("{#{k}, datetime}", t.strftime("%a %b %d %I:%M:%S%p"))
+          value.gsub!("{#{k}, datetime}", t.strftime('%a %b %d %I:%M:%S%p')) # it may show different time than written in doc due to timezone difference
         end
       end
     end
@@ -41,7 +38,7 @@ class Copy
 
   def get_data(params)
     data = get_file_data
-    data = data.select{|k, v| v[:updated_at] >= params[:since].to_i } if params[:since].present?
+    data = data.select { |_k, v| v[:updated_at] >= params[:since].to_i } if params[:since].present?
     data
   end
 
@@ -49,10 +46,8 @@ class Copy
 
   def get_file_data
     if $file_data.present?
-      puts "---IN"
       $file_data
     else
-      puts "---IN 2"
       file = File.read('./copy.json')
       JSON.parse(file).with_indifferent_access
     end
